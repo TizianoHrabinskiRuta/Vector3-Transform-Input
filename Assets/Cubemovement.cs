d9b2debf1d90a3ae615de4eeb470a3129d7d8698
Tiziano Hrabinski Ruta <101399093+TizianoHrabinskiRuta@users.noreply.github.com>
2022-05-17 14:07:39 -0300

    The funny has been achieved

---
+++ Assets/Cubemovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubemovement : MonoBehaviour
+{
+
+    public Vector3 CubeMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += CubeMovement; // the funny
+    }
+}
