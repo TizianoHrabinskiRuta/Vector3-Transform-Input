a4aeeb933c0bf04489efd88e05901ae8b6fcefae
Tiziano Hrabinski Ruta <101399093+TizianoHrabinskiRuta@users.noreply.github.com>
2022-05-17 14:28:14 -0300

    forgor again :skull:

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MovementSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(MovementSpeed, 0,0);
+        }       
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-MovementSpeed, 0,0);
+        }
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0, MovementSpeed);
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0,0, -MovementSpeed);
+        }
+    }
+}
